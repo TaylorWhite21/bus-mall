144dd6a005fc8325158e6950e80be264d7d5461e
TaylorWhite21 <taylorwhite2190@gmail.com>
2021-05-29 17:34:19 -0400

    added HTML and styling

--- js/app.js
+++ js/app.js
@@ -1,1 +1,14 @@
-'use strict'
+'use strict';
+
+
+// Features
+  // constructor: name, times image was shown, file path, number of times a pic was clicked
+  // image generator to show 3 images
+  // count how many times an image has been displayed
+  // event listener for the images that activates on click
+  // once image is clicked, show 3 new images
+  // create x amount of rounds that the images go through
+  // once x amount of rounds has been achieved, remove image event listeners
+  // once survey has been completed, display a view results button that when clicked displays all products and shows the votes it recieved, and times it was viewed
+
+
